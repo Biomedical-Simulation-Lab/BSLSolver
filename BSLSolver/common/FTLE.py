d9d523fcef844bf12477c2ead7c067793aae88c4
Anna Haley <anna.lk.haley@gmail.com>
2023-06-19 11:50:25 -0400

    ftle added without print or usage

---
+++ BSLSolver/common/FTLE.py
@@ -0,0 +1,32 @@
+from dolfin import *
+from oasis.common import utilities
+
+def ftle(mesh, V, u, original_bcs, dt):
+    new_bcs = []
+    for i, bc in enumerate(original_bcs):
+        subdomain = bc.user_sub_domain()
+        new_bcs.append(DirichletBC(V, Constant(0), subdomain))
+
+    CG1 = FunctionSpace(mesh, "CG", 1)
+    #get the trajectories
+    F = grad(u) + Identity(len(self.u))
+    #calculate the right Cauchy Green Tensor
+    C = F.T*F
+    #Get the eigenvalues of the C tensor
+    eigensolver = SLEPcEigenSolver(C)
+    print "Computing eigenvalues of the Right Cauchy-Green Tensor"
+    eigensolver.solve()
+    #get the maximum eigenvalues
+    eigens, _, _, _ = eigensolver.get_eigenpair(0) #not expecting any complex eigs
+    #project the eigenvalues to a CG1 space
+    eigs = OasisFunction(eigens, CG1, bcs=self.new_bcs, name='eigs',
+                            method='default', solver_type='bicgstab',
+                            preconditioner_type='jacobi')
+    eigs()
+    ftLe = 1/dt * ln(eigs**(1/2))
+    
+    #now just need to print to xdmffile
+
+
+
+
