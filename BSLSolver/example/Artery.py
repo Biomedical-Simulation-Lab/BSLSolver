8eaa902802e20102ae2730667f2058f33439ee66
Anna Haley <anna.lk.haley@gmail.com>
2023-06-27 10:03:36 -0400

    MPI in h5io

--- BSLSolver/example/Artery.py
+++ BSLSolver/example/Artery.py
@@ -691,12 +691,12 @@ def temporal_hook(u_, p_, p, q_, V, mesh, tstep, compute_flux,
     #OLD: if current_cycle == total_cycles-1:
     if (current_cycle > 0) and (current_cycle <= total_cycles-1): #
         if tstep % store_data == 0:	
-            #h5stdio.Save( current_cycle, t, tstep, Q_ins, Q_outs, NS_parameters, 'Step-%06d'%tstep, q_)#, int(MPI.comm_world.underlying_comm()) ) #multiple nodes?
+            h5stdio.Save( current_cycle, t, tstep, Q_ins, Q_outs, NS_parameters, 'Step-%06d'%tstep, q_)#, int(MPI.comm_world.underlying_comm()) ) #multiple nodes?
             #save ftle field
             if NS_parameters['save_ftle']:
                 FTLE.get_ftle(ftLe_forward, ftLe_backward, ftle_f, tstep)
-            #if mpi_rank == 0:
-            #    h5stdio.SaveXDMF( os.path.join(NS_parameters['folder'], NS_parameters['case_fullname']+'.xdmf') )
+            if mpi_rank == 0:
+                h5stdio.SaveXDMF( os.path.join(NS_parameters['folder'], NS_parameters['case_fullname']+'.xdmf') )
 
 def theend_hook(stop, newfolder, folder, save_ftle):
     if mpi_rank == 0:
