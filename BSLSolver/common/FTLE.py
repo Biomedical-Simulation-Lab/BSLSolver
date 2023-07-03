4afa7ce3e5e3ca3886cf9e4db5f9e316fbbdcd85
Anna Haley <anna.lk.haley@gmail.com>
2023-07-03 18:26:18 -0400

    ufl bug

--- BSLSolver/common/FTLE.py
+++ BSLSolver/common/FTLE.py
@@ -7,7 +7,7 @@ _krylov_solver=dict(
         solver_type='bicgstab',
         preconditioner_type='jacobi')
 
-components = ['x','y','z']
+components = [str(x) for x in range(3)]
 
 def eigenstate(A):
     """Eigenvalues and eigenprojectors of the 3x3 (real-valued) tensor A.
@@ -82,7 +82,7 @@ def get_ftle(ftLe_backward, ftLe_forward, ftLe_intersect, grad_sig, mesh, ftle_f
         print('Finished finding ftLe fields in %f s'%t.elapsed()[0])
 
     #get Hessian matrix of backward (attracting ftle)
-    _grad_sig  = as_vector([grad_sig[ui](ftLe_backward) for ui in components])
+    _grad_sig  = as_vector((grad_sig['0'](ftLe_backward), grad_sig['1'](ftLe_backward), grad_sig['2'](ftLe_backward)))
     hess = grad(_grad_sig) #ufl Hessian matrix DG0
     #get minimum eigenvector
     _, e_min_DG0 = eigenstate(hess) #the Hessian should always have real eigenvalues for any real function such as the ftle field
